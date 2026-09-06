bb6ec40e04223335c5ef98e102d60d3b146d5694
Christian Lopez <cjavier93@gmail.com>
2026-09-05 22:51:17 -0600

    fix: syntax error in formatCurrency

--- src/views/TransactionsView.vue
+++ src/views/TransactionsView.vue
@@ -180,8 +180,6 @@ const getAmountPrefix = (type: number | string) => {
 const formatCurrency = (amount: number) => {
   return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(amount)
 }
-  return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(amount)
-}
 
 const formatDate = (dateString: string) => {
   return new Date(dateString).toLocaleDateString()
